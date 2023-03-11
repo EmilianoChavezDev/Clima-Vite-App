2da2137248d2115b1257f89fcf99d7b73c16008c
EmilianoChavezDev <eemirandachavez@gmail.com>
2023-03-11 17:35:17 -0300

    Primer y ultimo commit

---
+++ src/components/Formulario.jsx
@@ -0,0 +1,48 @@
+import { useState } from "react";
+import useClima from "../hooks/useClima";
+import { PAISES } from "../constans";
+
+const Formulario = () => {
+  const [alerta, setAlerta] = useState("");
+  const { busqueda, datosBusqueda, consultarClima } = useClima();
+  const { ciudad, pais } = busqueda;
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    Object.values(busqueda).includes("")
+      ? setAlerta("Todos los campos son obligatorios")
+      : consultarClima(busqueda);
+  };
+
+  return (
+    <div className="contenedor">
+      {alerta && <p>{alerta}</p>}
+      <form onSubmit={handleSubmit}>
+        <div className="campo">
+          <label htmlFor="ciudad">Ciudad</label>
+          <input
+            type="text"
+            id="ciudad"
+            placeholder="Ingrese una Ciudad"
+            name="ciudad"
+            onChange={datosBusqueda}
+            value={ciudad}
+          />
+        </div>
+        <div className="campo">
+          <label htmlFor="pais">Pais</label>
+          <select id="pais" name="pais" onChange={datosBusqueda} value={pais}>
+            <option value="">-- Seleecione un pais --</option>
+            {PAISES.map((pais) => (
+              <option key={pais.value}>{pais.nombre}</option>
+            ))}
+          </select>
+        </div>
+
+        <input type="submit" value="Consultar Clima" />
+      </form>
+    </div>
+  );
+};
+
+export default Formulario;
---
+++ src/constans/index.js
@@ -0,0 +1,8 @@
+export const PAISES = [
+    { id: 1, value: "US", nombre: "Estados Unidos" },
+    { id: 2, value: "AR", nombre: "Argentina" },
+    { id: 3, value: "MX", nombre: "Mexico" },
+    { id: 4, value: "EN", nombre: "Inglaterra" },
+    { id: 5, value: "PY", nombre: "Paraguay" },
+    { id: 6, value: "UY", nombre: "Uruguay" },
+]
